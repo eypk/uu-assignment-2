77edc581bc5de548c22f0074b560dc016d70bcfc
eyup.kirci <eyup.kirci@finspiretech.com>
2023-03-12 18:15:58 +0100

    content finished: footer ve header left

--- info.js
+++ info.js
@@ -71,151 +71,115 @@ class Actor extends Artist {
 
 // Entity to be created using constructor
 
-const actorTom = new Actor(
-  "Mission Impossible 1",
-  "Tom Cruise",
-  1962,
-  [
-    "The Last Samurai (2003)",
-    "Mission Impossible 1-7 (1996 - 2023)",
-    "Jack Reacher 1-2 (2012, 2016)",
-    "Top Gun 1-2 (1986, 2022)",
-    "...etc, 50+",
-  ],
-  "https://nypost.com/wp-content/uploads/sites/2/2022/10/newspress-collage-24181120-1665303870946.jpg?quality=75&strip=all&1665289530&w=744"
-);
-
-const actorJon = new Actor(
-  "Mission Impossible 1",
-  "Jon Voight",
-  1938,
-  [
-    "Midnight Cowboy (1969)",
-    "Enemy of the State (1998)",
-    "Deliverance (1972)",
-    "Runaway Train (1985)",
-    "Transformers (2007)",
-  ],
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Jon_Voight_2012.jpg/440px-Jon_Voight_2012.jpg"
-);
-
-const actorReno = new Actor(
-  "Mission Impossible 1",
-  "Jean Reno",
-  1948,
-  [
-    "Leon (1994)",
-    "Ronin (1998)",
-    "Pink Panter (2006)",
-    "The Big Blue (2006)",
-    "...etc, 110+",
-  ],
-  "https://upload.wikimedia.org/wikipedia/commons/e/e3/Jean_Reno_Cannes_2016.jpg"
-);
-const actorEmmanuelle = new Actor(
-  "Mission Impossible 1",
-  "Emmanuelle Béart",
-  1963,
-  [
-    "8 Women (2022)",
-    "Torment (1994)",
-    "Nelly & Monsieur Arnaud (1995)",
-    "The Passengers of the Night (2022)",
-  ],
-  "https://m.media-amazon.com/images/M/MV5BMjExNDQ4ODU2Ml5BMl5BanBnXkFtZTYwMjI0ODgz._V1_.jpg"
-);
-
-const directorDePalma = new Director(
-  "Mission Impossible",
-  "Brian De Palma",
-  1940,
-  [
-    "Domino (2019)",
-    "Passion (2012)",
-    "Redacted(2007)",
-    "The Black Dahlia (2006) ",
-  ],
-  "https://resizing.flixster.com/FbkvPvuxxhrPo2OQuvkQu-xdTVQ=/218x280/v2/https://flxt.tmsimg.com/assets/72901_v9_bb.jpg"
-);
-
-const writerBruce = new Writer(
-  "Mission Impossible",
-  "Bruce Bernard Geller",
-  1930,
-  [
-    "Zane Grey Theater",
-    "Have Gun",
-    "Will Travel",
-    "The Rebel",
-    "Mission Impossible",
-  ],
-  "https://images.mubicdn.net/images/cast_member/58314/cache-284230-1511853223/image-w856.jpg?size=240x"
-);
-const writerDavid = new Writer(
-  "Mission Impossible",
-  "David Koepp",
-  1963,
-  ["Jurasic Park", "Stir of Echoes", "Ghost Town"],
-  "https://images.gr-assets.com/authors/1567797284p5/5620.jpg"
-);
-const writerSteven = new Writer(
-  "Mission Impossible",
-  "Steven Zaillian",
-  1953,
-  [
-    "A Civil Action",
-    "The Girl with the Dragon Tattoo",
-    "MoneyBall",
-    "Searching for Bobby Fischer",
-  ],
-  "https://www.themoviedb.org/t/p/w500/3V8X2EnjzqdrUagdnjTgKi213AJ.jpg"
-);
+//moive object
 
 const movieMI = new Movie(
   "Mission Impossible",
   "Action",
   1996,
-  "http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcRKJNiOaWFCkcb2qQ_zMitV7QpsYiWgDNjZVWmQEsLxAMkrLr_2MsZXY3-wUIPLPC_hV8Q3Y-Fp_vmK9XA",
-  "https://m.imdb.com/video/vi3226468377/?playlistId=tt0117060&ref_=tt_pr_ov_vi",
+  "https://www.themoviedb.org/t/p/w1280/l5uxY5m5OInWpcExIpKG6AR3rgL.jpg",
+  "assets/m-i-v.mp4",
   "An American agent, under false suspicion of disloyalty, must discover and expose the real spy without the help of his organization.",
-  [directorDePalma],
-  [writerBruce, writerDavid, writerSteven],
-  [actorTom, actorReno, actorJon, actorEmmanuelle]
+  [
+    new Director(
+      "Mission Impossible",
+      "Brian De Palma",
+      1940,
+      [
+        "Domino (2019)",
+        "Passion (2012)",
+        "Redacted(2007)",
+        "The Black Dahlia (2006) ",
+      ],
+      "https://resizing.flixster.com/FbkvPvuxxhrPo2OQuvkQu-xdTVQ=/218x280/v2/https://flxt.tmsimg.com/assets/72901_v9_bb.jpg"
+    ),
+  ],
+  [
+    new Writer(
+      "Mission Impossible",
+      "Bruce Bernard Geller",
+      1930,
+      [
+        "Zane Grey Theater",
+        "Have Gun",
+        "Will Travel",
+        "The Rebel",
+        "Mission Impossible",
+      ],
+      "https://images.mubicdn.net/images/cast_member/58314/cache-284230-1511853223/image-w856.jpg?size=240x"
+    ),
+    new Writer(
+      "Mission Impossible",
+      "David Koepp",
+      1963,
+      ["Jurasic Park", "Stir of Echoes", "Ghost Town"],
+      "https://images.gr-assets.com/authors/1567797284p5/5620.jpg"
+    ),
+    new Writer(
+      "Mission Impossible",
+      "Steven Zaillian",
+      1953,
+      [
+        "A Civil Action",
+        "The Girl with the Dragon Tattoo",
+        "MoneyBall",
+        "Searching for Bobby Fischer",
+      ],
+      "https://www.themoviedb.org/t/p/w500/3V8X2EnjzqdrUagdnjTgKi213AJ.jpg"
+    ),
+  ],
+  [
+    new Actor(
+      "Mission Impossible 1",
+      "Tom Cruise",
+      1962,
+      [
+        "The Last Samurai (2003)",
+        "Mission Impossible 1-7 (1996 - 2023)",
+        "Jack Reacher 1-2 (2012, 2016)",
+        "Top Gun 1-2 (1986, 2022)",
+      ],
+      "https://www.themoviedb.org/t/p/w500/8qBylBsQf4llkGrWR3qAsOtOU8O.jpg"
+    ),
+    new Actor(
+      "Mission Impossible 1",
+      "Jean Reno",
+      1948,
+      [
+        "Leon (1994)",
+        "Ronin (1998)",
+        "Pink Panter (2006)",
+        "The Big Blue (2006)",
+      ],
+      "https://upload.wikimedia.org/wikipedia/commons/e/e3/Jean_Reno_Cannes_2016.jpg"
+    ),
+    new Actor(
+      "Mission Impossible 1",
+      "Jon Voight",
+      1938,
+      [
+        "Midnight Cowboy (1969)",
+        "Enemy of the State (1998)",
+        "Deliverance (1972)",
+        "Runaway Train (1985)",
+      ],
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Jon_Voight_2012.jpg/440px-Jon_Voight_2012.jpg"
+    ),
+    new Actor(
+      "Mission Impossible 1",
+      "Emmanuelle Béart",
+      1963,
+      [
+        "8 Women (2022)",
+        "Torment (1994)",
+        "Nelly & Monsieur Arnaud (1995)",
+        "The Passengers of the Night (2022)",
+      ],
+      "https://m.media-amazon.com/images/M/MV5BMjExNDQ4ODU2Ml5BMl5BanBnXkFtZTYwMjI0ODgz._V1_.jpg"
+    ),
+  ]
 );
 console.log(movieMI);
-
-//PAGE SECTIONS
-
-// body
-const body = document.querySelector("body");
-body.style.boxSizing = "border-box";
-body.style.margin = "0px";
-body.style.padding = "0px";
-
-// body > header
-const header = document.createElement("header");
-header.style.height = "10vh";
-header.style.padding = "1rem";
-body.appendChild(header);
-
-// body > Main
-const articleSection = document.createElement("article");
-articleSection.classList = "article";
-articleSection.style.padding = "1rem";
-articleSection.style.height = "40vh";
-body.appendChild(articleSection);
-
-// body > Main > Section
-const movieSection = document.createElement("section");
-movieSection.style.height = "40vh";
-movieSection.style.padding = "2rem";
-body.appendChild(movieSection);
-
-// body > Footer
-const footer = document.createElement("footer");
-footer.style.height = "10vh";
-footer.style.padding = "1rem";
-body.appendChild(footer);
 
 //FUNCTIONS
 
@@ -233,12 +197,12 @@ const createSectionMenu = () => {
   sectionOption2.text = "header";
 
   const sectionOption3 = document.createElement("option");
-  sectionOption3.value = "article";
-  sectionOption3.text = "article";
+  sectionOption3.value = "section";
+  sectionOption3.text = "section";
 
   const sectionOption4 = document.createElement("option");
-  sectionOption4.value = "section";
-  sectionOption4.text = "section";
+  sectionOption4.value = "article";
+  sectionOption4.text = "article";
 
   const sectionOption5 = document.createElement("option");
   sectionOption5.value = "footer";
@@ -259,12 +223,12 @@ const createStyleMenu = () => {
   selectStyleBox.classList = "menu selectStyleBox";
 
   const option1 = document.createElement("option");
-  option1.value = "fontSize";
-  option1.text = "Enlarge FontSize";
+  option1.value = "Delarge";
+  option1.text = "Delarge FontSize";
 
   const option2 = document.createElement("option");
-  option2.value = "imageSize";
-  option2.text = "Enlarge ImageSize";
+  option2.value = "Enlarge";
+  option2.text = "Enlarge FontSize";
 
   const option3 = document.createElement("option");
   option3.value = "DarkMode";
@@ -282,7 +246,7 @@ const createStyleMenu = () => {
   return selectStyleBox;
 };
 
-// select sction
+// select section
 const SelectSection = (e) => {
   let section = e.target.value;
 
@@ -315,12 +279,12 @@ const ChangeStyle = (e) => {
   let appliedSection = document.querySelector(selectedSection);
 
   switch (selectedStyle) {
-    case "fontSize":
+    case "Enlarge":
       appliedSection.style.fontSize = "1.5rem";
       break;
 
-    case "imageSize":
-      appliedSection.style.fontSize = "1.5rem";
+    case "Delarge":
+      appliedSection.style.fontSize = "1rem";
       break;
 
     case "DarkMode":
@@ -334,87 +298,6 @@ const ChangeStyle = (e) => {
       break;
   }
 };
-
-//Insert Movie Info
-
-const movieHeader = document.createElement("h2");
-movieHeader.innerText = movieMI.filmTitle;
-articleSection.appendChild(movieHeader);
-
-const movieYear = document.createElement("span");
-movieYear.classList = "year";
-movieYear.innerText = movieMI.filmYear;
-articleSection.appendChild(movieYear);
-
-const movieGenre = document.createElement("span");
-movieGenre.innerText = movieMI.filmGenre;
-movieGenre.classList = "genre";
-articleSection.appendChild(movieGenre);
-
-const moviePlot = document.createElement("p");
-moviePlot.innerText = movieMI.filmPlot;
-articleSection.appendChild(moviePlot);
-
-//movie Director
-
-const movieDirector = document.createElement("div");
-
-let directorText = document.createElement("span");
-directorText.innerText = "Director:";
-movieDirector.appendChild(directorText);
-
-movieMI.filmDirector.map((artist, index) => {
-  let star = document.createElement("span");
-  star.id = index;
-  star.classList = `tooltip-container movieDirector ${artist.artistName}`;
-  star.style.margin = "0px 5px";
-  star.innerText = artist.artistName;
-  movieDirector.appendChild(star);
-});
-
-articleSection.appendChild(movieDirector);
-
-//MovieWriter
-
-const movieWriter = document.createElement("div");
-
-let writerText = document.createElement("span");
-writerText.innerText = "Writer:";
-movieWriter.appendChild(writerText);
-
-movieMI.filmWriters.map((artist, index) => {
-  let star = document.createElement("span");
-  star.id = index;
-  star.classList = `tooltip-container movieWriter ${artist.artistName}`;
-  star.style.margin = "0px 5px";
-  star.innerText = artist.artistName;
-  movieWriter.appendChild(star);
-});
-
-articleSection.appendChild(movieWriter);
-
-//movie Stars
-const movieActors = document.createElement("div");
-
-let movieStars = document.createElement("span");
-movieStars.innerText = "Stars:";
-movieActors.appendChild(movieStars);
-
-movieMI.filmStars.map((artist, index) => {
-  let star = document.createElement("span");
-  star.id = index;
-  star.classList = `tooltip-container movieStar ${artist.artistName}`;
-  star.style.margin = "0px 5px";
-  star.innerText = artist.artistName;
-  movieActors.appendChild(star);
-});
-
-articleSection.appendChild(movieActors);
-
-//tooltip events
-
-articleSection.addEventListener("mouseover", (e) => insertTooltip(e));
-articleSection.addEventListener("mouseout", (e) => deleteTooltip(e));
 
 //create tooltip
 
@@ -442,19 +325,16 @@ const insertTooltip = (e) => {
       //div.tooltip
       let tooltip = document.createElement("div");
       tooltip.classList = "tooltipBox";
-      tooltip.style.position = "absolute";
-      tooltip.style.right = "5%";
 
       let img = document.createElement("img");
       imgUrl = artist[0].image;
       img.setAttribute("src", imgUrl);
       img.setAttribute("alt", "artist image");
-      img.width = 280;
       tooltip.appendChild(img);
 
       let p1 = document.createElement("p");
+      p1.classList = "artist-name-tooltip";
       p1.innerText = artist[0].artistName;
-      p1.style.fontWeight = "bold";
       tooltip.appendChild(p1);
 
       let p2 = document.createElement("p");
@@ -462,7 +342,6 @@ const insertTooltip = (e) => {
       tooltip.appendChild(p2);
 
       let ul = document.createElement("ul");
-      ul.style.padding = "0px 15px";
       let list = artist[0].movieList ? artist[0].movieList : artist[0].books;
 
       for (let i = 0; i < list.length; i++) {
@@ -484,14 +363,137 @@ const deleteTooltip = (e) => {
   } catch (error) {}
 };
 
-// menu events
-
-header.addEventListener("change", (e) => SelectSection(e));
-header.addEventListener("change", (e) => ChangeStyle(e));
-
 // ONLOAD
 window.onload = () => {
-  console.log("page is fully loaded");
+  //PAGE SECTIONS
+
+  // body
+  const body = document.querySelector("body");
+
+  // body > header
+  const header = document.createElement("header");
   header.appendChild(createSectionMenu());
   header.appendChild(createStyleMenu());
+  body.appendChild(header);
+
+  // body > Section
+  const movieSection = document.createElement("section");
+  body.appendChild(movieSection);
+
+  // movie poster
+  const poster = document.createElement("img");
+  poster.classList = "poster";
+  poster.setAttribute("src", movieMI.filmPoster);
+  poster.setAttribute("alt", "movie poster");
+  movieSection.appendChild(poster);
+
+  //movie  thriller
+
+  const video = document.createElement("video");
+  video.classList = "thriller";
+  video.setAttribute("src", movieMI.filmTrailer);
+  video.setAttribute("autoplay", true);
+  video.setAttribute("muted", true);
+  video.setAttribute("controls", true);
+  video.setAttribute("alt", "movie thriller");
+
+  movieSection.appendChild(video);
+
+  // body > Article
+  const articleSection = document.createElement("article");
+  body.appendChild(articleSection);
+
+  //Insert Movie Info to Article
+
+  /// title, year, genre
+
+  const movieHeader = document.createElement("h2");
+  movieHeader.innerText = movieMI.filmTitle;
+  articleSection.appendChild(movieHeader);
+
+  const movieYear = document.createElement("span");
+  movieYear.classList = "year";
+  movieYear.innerText = movieMI.filmYear;
+  articleSection.appendChild(movieYear);
+
+  const movieGenre = document.createElement("span");
+  movieGenre.classList = "genre";
+  movieGenre.innerText = movieMI.filmGenre;
+  articleSection.appendChild(movieGenre);
+
+  const moviePlot = document.createElement("p");
+  moviePlot.innerText = movieMI.filmPlot;
+  articleSection.appendChild(moviePlot);
+
+  /// director
+
+  const movieDirector = document.createElement("div");
+
+  let directorText = document.createElement("span");
+  directorText.innerText = "Director:";
+  movieDirector.appendChild(directorText);
+
+  movieMI.filmDirector.map((artist) => {
+    let star = document.createElement("span");
+    star.classList = `tooltip-container movieDirector ${artist.artistName}`;
+    star.innerText = artist.artistName;
+    movieDirector.appendChild(star);
+  });
+
+  articleSection.appendChild(movieDirector);
+
+  /// writer
+
+  const movieWriter = document.createElement("div");
+
+  let writerText = document.createElement("span");
+  writerText.innerText = "Writer:";
+  movieWriter.appendChild(writerText);
+
+  movieMI.filmWriters.map((artist) => {
+    let star = document.createElement("span");
+    star.classList = `tooltip-container movieWriter ${artist.artistName}`;
+    star.innerText = artist.artistName;
+    movieWriter.appendChild(star);
+  });
+
+  articleSection.appendChild(movieWriter);
+
+  /// stars
+  const movieActors = document.createElement("div");
+
+  let movieStars = document.createElement("span");
+  movieStars.innerText = "Stars:";
+  movieActors.appendChild(movieStars);
+
+  movieMI.filmStars.map((artist) => {
+    let star = document.createElement("span");
+    star.classList = `tooltip-container movieStar ${artist.artistName}`;
+    star.innerText = artist.artistName;
+    movieActors.appendChild(star);
+  });
+
+  articleSection.appendChild(movieActors);
+
+  // body > Footer
+  const footer = document.createElement("footer");
+  footer.appendChild(createSectionMenu());
+  footer.appendChild(createStyleMenu());
+  body.appendChild(footer);
+
+  //EVENTS
+
+  //tooltip events
+
+  articleSection.addEventListener("mouseover", (e) => insertTooltip(e));
+  articleSection.addEventListener("mouseout", (e) => deleteTooltip(e));
+
+  // menu events
+
+  header.addEventListener("change", (e) => SelectSection(e));
+  header.addEventListener("change", (e) => ChangeStyle(e));
+  footer.addEventListener("change", (e) => SelectSection(e));
+  footer.addEventListener("change", (e) => ChangeStyle(e));
+
+  console.log("page is fully loaded");
 };
